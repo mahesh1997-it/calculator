a61eff8b8fa27ceaaee87761aa4d9d8be5c3ad84
mahesh1997-it <66937811+mahesh1997-it@users.noreply.github.com>
2020-07-12 09:35:00 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,12 @@
+function display(num)
+{
+    document.getElementById("do").value=document.getElementById("do").value+num;
+}
+function e()
+{
+    document.getElementById("do").value=eval(document.getElementById("do").value);
+}
+function C()
+{
+    document.getElementById("do").value="";
+}
